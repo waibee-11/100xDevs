069eb205e39d8897b3abf645de61fdad1020759d
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-01-25 16:48:32 +0400

    DOM added

---
+++ Week 04 100xD/DOM/script.js
@@ -0,0 +1,16 @@
+// METHOD 1:
+// function populateDiv(){
+//     const element = document.getElementById("final-sum");
+//     const num1 = document.getElementById("num1");
+//     const num2 = document.getElementById("num2");
+//     element.innerHTML = parseInt(num1.value)+ parseInt(num2.value);
+// }
+
+// METHOD 2: HITTING THE BACKEND
+async function populateDiv(){
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const response = await fetch(`https://sum-server.100xdevs.com/sum?a=${num1}&b=${num2}`);
+    const ans = await response.text();
+    document.getElementById("final-sum").innerHTML = ans;
+}
