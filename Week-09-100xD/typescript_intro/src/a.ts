4e594575deec386331611200dad9ee2e24ca3d02
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-03-16 19:32:01 -0400

    ts

--- Week-09-100xD/typescript_intro/src/a.ts
+++ Week-09-100xD/typescript_intro/src/a.ts
@@ -1,2 +1,9 @@
-const x: number = 11;
-console.log(x);
+function identity<T>(arg: T): T{
+    return arg;
+}
+
+let x: string = identity<string>("yash");
+let y: number = identity<number>(1);
+
+console.log(x.toUpperCase());
+console.log(y);
