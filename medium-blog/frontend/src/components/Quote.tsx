4c54f33259b69b54e6271f29baa903dcccc8914f
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-03-21 03:48:27 -0400

    responsive

--- medium-blog/frontend/src/components/Quote.tsx
+++ medium-blog/frontend/src/components/Quote.tsx
@@ -1,7 +1,7 @@
 export function Quote() {
   return (
     <div className="flex justify-center h-screen bg-slate-100 flex-col pl-44 gap-10">
-        <div className="text-8xl font-bold pl-10 bg-black text-white mr-20 py-2 rounded-lg font-lora">Medium</div>
+        <div className="text-7xl font-bold pl-10 bg-black text-white mr-20 py-2 rounded-lg font-lora">Medium</div>
         <div className="flex justify-center flex-col border-l border-t border-slate-600 pl-10 mr-20 py-10">
             <div className="max-w-lg text-8xl font-bold font-inter">WRITE</div>
             <div className="max-w-lg text-8xl font-bold font-inter">YOUR</div>
