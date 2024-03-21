9e5c21829f55b6cf7c6534c4bc40b6d8e7b05971
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-03-21 04:04:37 -0400

    ui touchup

--- medium-blog/frontend/src/pages/Blogs.tsx
+++ medium-blog/frontend/src/pages/Blogs.tsx
@@ -22,7 +22,7 @@ export function Blogs() {
         <Appbar userName={userName} />
         <div className="grid grid-cols-12 h-full">
             <div className="xl:col-span-1"></div>
-            <div className="xl:mx-0 mx-10 col-span-12 xl:col-span-10 flex flex-col py-10 h-full justify-start flex-wrap">
+            <div className="xl:mx-0 mx-5 col-span-12 xl:col-span-10 flex flex-col py-10 h-full justify-start flex-wrap">
             {blogs.reverse().map((blog) => <BlogCard name={blog.author.name || "Anonymous"} title={blog.title} content={blog.content} id={blog.id} coverImage={blog.coverImage} profileImage={blog.author.image} postDate={blog.postDate} />
             )}
             </div>
