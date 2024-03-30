d05cf364b3d15b668b2b847c8b9d01fcf5bc95b9
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-03-29 20:22:34 -0400

    week-14

--- week-14/first-next-app/app/(auth)/signin/page.tsx
+++ week-14/first-next-app/app/(auth)/signin/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 export default function Signin() {
   return <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -11,7 +12,9 @@ export default function Signin() {
                   <div className="pt-2">
                       <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                       <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                      <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                      <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={() => {
+                        console.log("Hello");
+                      }}>Sign in</button>
                   </div>
               </div>
           </a>
